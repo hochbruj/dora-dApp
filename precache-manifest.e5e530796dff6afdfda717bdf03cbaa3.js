17a8b7acbd1fa90ae9b8a898cf56e16e3fc33947
Jan Hochbrueckner <jhochbrueckner@gmail.com>
2019-11-11 19:39:01 +1100

    Updates

---
+++ precache-manifest.e5e530796dff6afdfda717bdf03cbaa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caa01e7916194b94e43e",
+    "url": "/dora-dApp/static/js/runtime~main.caa01e79.js"
+  },
+  {
+    "revision": "2350081415814eaee771",
+    "url": "/dora-dApp/static/js/main.23500814.chunk.js"
+  },
+  {
+    "revision": "90c50bf798b72f9f6e6c",
+    "url": "/dora-dApp/static/js/1.90c50bf7.chunk.js"
+  },
+  {
+    "revision": "2350081415814eaee771",
+    "url": "/dora-dApp/static/css/main.186c9718.chunk.css"
+  },
+  {
+    "revision": "3cfdb90249b5f7112e387507d07fab3d",
+    "url": "/dora-dApp/index.html"
+  }
+];
